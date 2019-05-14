5215355431a7c3b970397677ebbd646a651a8847
Dapo Aderemi <adedapopaul@yahoo.com>
2019-05-14 12:57:35 +0100

    edit app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
-var port = process.env.port || 3000 
+var port = process.env.PORT || 3000 
 
 var mongoose = require('mongoose')  // this is the same as var mongoose = require('mongoose')
 
